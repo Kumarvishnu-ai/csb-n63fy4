6bdde78efe06561ea5fe75e2265363911027b597
Code Sandbox <hello@codesandbox.io>
2023-06-12 18:14:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.03e11a153775a29508990db4ba8fa71c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4826bb41c9b370cf6955",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/css/main.89ed9f2b.chunk.css"
+  },
+  {
+    "revision": "4826bb41c9b370cf6955",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/js/main.fb39ed07.chunk.js"
+  },
+  {
+    "revision": "edf134a90b1897e89f8e",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/js/runtime~main.cf9e349c.js"
+  },
+  {
+    "revision": "8d794ca64b2b4e616544",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/js/2.097e056b.chunk.js"
+  },
+  {
+    "revision": "5e4db5272f8a48d5505821f0fb762833",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/media/image_correct.5e4db527.jpg"
+  },
+  {
+    "revision": "0b1f3e62b46ab75ae3ea73edf8f65063",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/media/scan_process.0b1f3e62.gif"
+  },
+  {
+    "revision": "0c7248855fa32cf64ed57349a6fb6df4",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/media/card_back@3x.0c724885.png"
+  },
+  {
+    "revision": "d268a33dd7020770c9be64f14571759a",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/media/illustration1@3x.d268a33d.png"
+  },
+  {
+    "revision": "406cb2b985fcbd5dca1111b268798326",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/media/illustration2@3x.406cb2b9.png"
+  },
+  {
+    "revision": "c6c0d9529f2ed5b233fc50de1c57feea",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/static/media/acuant-logo@2x@3x.c6c0d952.png"
+  },
+  {
+    "revision": "d5ef5d95c9ddcbf1ccfe847db15e68a8",
+    "url": "https://kumarvishnu-ai.github.io/csb-n63fy4/index.html"
+  }
+];
